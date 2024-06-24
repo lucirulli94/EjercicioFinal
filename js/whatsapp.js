0d3e6621cabf0fad04e77d662caae2d27db19f47
lucirulli94 <luciana.cirulli@usal.edu.ar>
2024-06-24 15:12:42 -0300

    Footer, new css rules

---
+++ js/whatsapp.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.floating-wpp').floatingWhatsApp({
+      phone: 'xxxxxxxxxx',
+      popupMessage: 'Popup Message',
+      showPopup: true,
+      message: 'Message To Send',
+      headerTitle: 'Header Title'
+    });
+  });
